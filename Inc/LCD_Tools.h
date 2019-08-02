20ba95fff1b3f4724740016de55d01e23d178f3e
Jakub Białkowski <jakub.bialkowski@fi.abb.com>
2019-08-02 15:31:18 +0200

    Fill display function corrected

--- Inc/LCD_Tools.h
+++ Inc/LCD_Tools.h
@@ -5,11 +5,16 @@
 #define DATA 0
 #define COMMAND 1
 
+#define PIXEL_ADDR_REG_1 0x2A
+#define PIXEL_ADDR_REG_2 0x2B
+#define PIXEL_ADDR_REG_3 0x2C
+
+
 
 
 void LCD_Data_Preparation(uint16_t Color);
-void Fill_display(void);
+void Fill_display(uint16_t Color);
 void LCD_Init_HW(void);
 void LCD_Configure(void);
 void LCD_Init(void);
-void Set_Address (void);
+void Set_Address (uint8_t Start_X, uint8_t End_X, uint8_t Start_Y, uint8_t End_Y);
--- Src/LCD_Tools.c
+++ Src/LCD_Tools.c
@@ -4,41 +4,25 @@
 #define SMALL_DISPLAY
 
 
-#ifdef SMALL_DISPLAY
-uint32_t Number_of_pixels = 128;
+#ifdef SMALL_DISPLAY									// 128x160 pixels
+uint32_t Num_of_pixels_row = 0x80; 		// 128d
+uint32_t Num_of_pixels_col = 0xA0; 		// 160d
 uint16_t LCD_Data[128];
 #endif
 
-#ifdef BIG_DISPLAY
-uint32_t Number_of_pixels = 76800;
-uint16_t LCD_Data[76800];
+#ifdef BIG_DISPLAY										 // 240x320 pixels
+uint32_t Number_of_pixels_row = 0xF0;  // 240d
+uint32_t Number_of_pixels_col = 0x140; // 320d
+uint16_t LCD_Data[240];
 #endif
 
 
-/* Fulfill LCD_Data array with Color */
-void LCD_Data_Preparation(uint16_t Color){
-	uint8_t i;
-	for(i=0;i<Number_of_pixels;i++){
-		LCD_Data[i] = Color;
-	}
-}
-
-
-/* Color mode is set as 16 bit/pixel. Data have to be 16-bit. */
-void Fill_display(void)
-{	
-	SPI_Send_Data_16bit(LCD_Data, Number_of_pixels);
-}
-
-
-/* Procedure to initialize LCD display */
+/* Procedure to run LCD HW */
 void LCD_Init_HW(void){
-	
 	DC_Pin(RES);
 	CS_Pin(RES);
 	RESX_Pin(RES);
 	HAL_Delay(5);
-	
 	RESX_Pin(SET);
 	HAL_Delay(5);
 	RESX_Pin(RES);
@@ -51,11 +35,7 @@ void LCD_Init_HW(void){
 
 
 /* Setup and configure displaying method*/
-void LCD_Configure(void)
-{
-
-	
-	
+void LCD_Configure(void){
 	// Configure display (data/commands to send)
 	uint8_t LcdInitVal_Array[74][2] = {
 	{COMMAND ,0x11},
@@ -147,72 +127,52 @@ void LCD_Configure(void)
 }
 
 
-
-void Set_Address (void){
-	uint8_t Address_1 = 0x2A;
-	uint8_t Dat_1 = 0;
-	uint8_t Dat_2 = 0x64;
+/* Function that is setting area to write */
+void Set_Address (uint8_t Start_X, uint8_t End_X, uint8_t Start_Y, uint8_t End_Y){
+	uint8_t Zero = 0;
 	
+	uint8_t Address_1 = PIXEL_ADDR_REG_1;
 	SPI_Send_Command_8bit(&Address_1);
-	SPI_Send_Data_8bit(&Dat_1);
-	SPI_Send_Data_8bit(&Dat_1);
-	SPI_Send_Data_8bit(&Dat_1);
-	SPI_Send_Data_8bit(&Dat_2);
+	SPI_Send_Data_8bit(&Zero);		// Starting point X-axis 1st byte - MSB XS
+	SPI_Send_Data_8bit(&Start_X);	// Starting point X-axis 2nd byte - LSB XS
+	SPI_Send_Data_8bit(&Zero);		// Ending point X-axis 1st byte - MSB XE
+	SPI_Send_Data_8bit(&End_X);		// Ending point X-axis 2nd byte - MSB XE
 	
-	uint8_t Address_2 = 0x2B;
+	uint8_t Address_2 = PIXEL_ADDR_REG_2;
 	SPI_Send_Command_8bit(&Address_2);
-	SPI_Send_Data_8bit(&Dat_1);
-	SPI_Send_Data_8bit(&Dat_1);
-	SPI_Send_Data_8bit(&Dat_1);
-	SPI_Send_Data_8bit(&Dat_2);
+	SPI_Send_Data_8bit(&Zero);		// Starting point Y-axis 1st byte - MSB XS
+	SPI_Send_Data_8bit(&Start_Y);	// Starting point Y-axis 2nd byte - LSB XS
+	SPI_Send_Data_8bit(&Zero);		// Ending point Y-axis 1st byte - MSB XE
+	SPI_Send_Data_8bit(&End_Y);		// Ending point Y-axis 2nd byte - MSB XE
 	
-	uint8_t Address_3 = 0x2C;
+	uint8_t Address_3 = PIXEL_ADDR_REG_3;
 	SPI_Send_Command_8bit(&Address_3);
 }
 
-
-
-void LCD_Init(void){
-	LCD_Init_HW();
-	HAL_Delay(10);
-	LCD_Configure();
-	LCD_Data_Preparation(0xFB01);
-	Set_Address();
-	MX_SPI2_Init_16BIT();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
-	Fill_display();
+/* Fulfill LCD_Data array with Color */
+void LCD_Data_Preparation(uint16_t Color){
+	uint8_t i;
+	for(i=0;i<Num_of_pixels_row;i++){
+		LCD_Data[i] = Color;
+	}
 }
 
 
+/* Color mode is set as 16 bit/pixel. Data have to be 16-bit. */
+void Fill_display(uint16_t Color){
+	uint8_t i = 0;
+	LCD_Data_Preparation(Color);
+	for(i=0 ; i<Num_of_pixels_col ; i++){
+		SPI_Send_Data_16bit(LCD_Data, Num_of_pixels_row);
+	}
+}
+
+/* LCD initialize procedure */
+void LCD_Init(void){
+	LCD_Init_HW();
+	HAL_Delay(5);
+	LCD_Configure();
+	Set_Address(0, Num_of_pixels_row, 0, Num_of_pixels_col);
+	MX_SPI2_Init_16BIT();
+	Fill_display(0xFF01);
+}
