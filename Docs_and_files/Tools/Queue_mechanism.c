6cc99f428e00eb389037341e83c7db5d320cf240
Jakubson <jakubbial@o2.pl>
2019-09-09 22:13:05 +0200

    [Queue] Queue prototype. Added Adding items to queue

--- Docs_and_files/Tools/Queue_mechanism.c
+++ Docs_and_files/Tools/Queue_mechanism.c
@@ -3,24 +3,36 @@
 
 
 struct Queue{
-	int* Item_array;
-	int* First_item;
-	int* Last_item;
-	int* Current_item;
+	int *Item_array;
+	int First_item;
+	int Last_item;
+	int Current_item;
+	int *Current_item_p;
 	int Queue_size;
 };
 
-void Initialize_queue_struct(struct Queue Queue_name, int Size)
+void Initialize_queue_struct(struct Queue *Queue_name, int Size)
 {
-	Queue_name.Item_array = NULL;
-	Queue_name.Queue_size = Size;
-	Queue_name.Item_array = malloc(Queue_name.Queue_size);	
+	Queue_name->Item_array = NULL;
+	Queue_name->Queue_size = Size;
+	Queue_name->Item_array = (int *)calloc(Size, sizeof(int));
+	Queue_name->Current_item = 0;
+	Queue_name->Current_item_p = NULL;
+	Queue_name->First_item = 0;
+	Queue_name->Last_item = Size;
 }
 
-void Add_Queue_Item(struct Queue Queue_name)
+void Add_Queue_Item(struct Queue *Queue_name, int Item)
 {
-	Queue_name.Item_array = 1;
-	
+	if(Queue_name->Current_item >= Queue_name->Queue_size)
+	{
+		printf("Queue full");
+	} else
+	{
+		*((Queue_name->Item_array)+Queue_name->Current_item) = Item;
+		Queue_name->Current_item += 1;
+		Queue_name->Current_item_p = Queue_name->Item_array + Queue_name->Current_item;
+	}
 }
 
 
@@ -28,6 +40,21 @@ void Add_Queue_Item(struct Queue Queue_name)
 main()
 {
 	struct Queue Kju1;
-	Initialize_queue_struct(Kju1, 10);
+	Initialize_queue_struct(&Kju1, 10);	
+	Add_Queue_Item(&Kju1, 5);
+	Add_Queue_Item(&Kju1, 8);
+	Add_Queue_Item(&Kju1, 55);
+	printf("First item: %d\nLast item: %d\nCurrent item: %d\nCurrent item pointer: %x\nFirst element pointer: %x\nCurrent item value: %d\n",
+		    Kju1.First_item, Kju1.Last_item, Kju1.Current_item, Kju1.Current_item_p, Kju1.Item_array, *(Kju1.Current_item_p-1));
+	int i;    
+	for(i = 0; i < 10; i++)
+	{
+		printf("Element number %d: %d, %x\n", i, *(Kju1.Item_array+i), Kju1.Item_array+i);
+	}
 }
+
+
+
+
+
 
--- Docs_and_files/Tools/pointers.c
+++ Docs_and_files/Tools/pointers.c
@@ -2,8 +2,35 @@
 #include <stdlib.h>
 
 
+struct Koleja{
+	int *tablica_wskaznik;
+};
+
+void Initialize(struct Koleja *Kolej)
+{
+	Kolej->tablica_wskaznik = (int *)calloc(10, sizeof(int));
+}
+
+void Add_element(struct Koleja *Kolej, int item)
+{
+	*Kolej->tablica_wskaznik = item;
+}
+
+
+
 main()
 {
+	struct Koleja Kol;
+	Initialize(&Kol);	
+	Add_element(&Kol, 12);
+	
+	//*Kol.tablica_wskaznik = 10;
+	//*(Kol.tablica_wskaznik+1) = 10;
+	
+	printf("Wskaznik: %x, Wartosc: %d\n", Kol.tablica_wskaznik, *(Kol.tablica_wskaznik));
+	
+	
+	/*
 	int *ass = (int *)calloc(10, sizeof(int));
 	
 	*ass = 1;
@@ -16,5 +43,6 @@ main()
 		val = *(ass++);
 		printf("Value of %d is: %d\n", i, val);
 	}
+	*/
 }
 
