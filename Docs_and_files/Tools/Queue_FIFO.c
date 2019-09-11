6e5bcf009009b53c0c40ae689703de474a093c8c
Jakub Białkowski <jakub.bialkowski@fi.abb.com>
2019-09-11 15:19:29 +0200

    [Queue] Added FIFO prototype

--- Docs_and_files/Tools/Queue_FIFO.c
+++ Docs_and_files/Tools/Queue_FIFO.c
@@ -5,24 +5,76 @@
 typedef struct Queue {
 	int Array_size;
 	int *First_array_element;
+	int *Current_element;
+	int *Next_element;
+	int Space_left;
 	
 } Queue_data;
 
+// Proto of printing queue
 void Print_Queue(Queue_data *Queue_name);
-
 
 void Initialize_queue(Queue_data *Queue_name, int Queue_size)
 {
 	Queue_name->Array_size = Queue_size;
 	Queue_name->First_array_element = calloc(Queue_size, sizeof(int));
+	Queue_name->Current_element=Queue_name->First_array_element;
+	Queue_name->Next_element = Queue_name->First_array_element;
+	Queue_name->Space_left = Queue_name->Array_size;
+}
+
+int Add_element(Queue_data *Queue_name, int Element)
+{
+	int Operation_status;
+	
+	printf("Space left: %d\n", Queue_name->Space_left);
+	
+	if(Queue_name->Space_left != 0)
+	{
+		*(Queue_name->Next_element) = Element;
+		
+		if(Queue_name->Space_left != 1)
+		{
+			Queue_name->Next_element += 1;
+		}
+		
+		Queue_name->Space_left -= 1;
+		Operation_status = 1;
+	}else
+	{
+		Operation_status = 0;
+	}
 }
 
 main()
 {
 	Queue_data d;
-	d.Array_size = 5;
+	Initialize_queue(&d, 4);
+	Add_element(&d, 150);
+	Add_element(&d, 250);	
+
 	Print_Queue(&d);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 void Print_Queue(Queue_data *Queue_name)
 {
@@ -30,14 +82,41 @@ void Print_Queue(Queue_data *Queue_name)
 	int Queue_size = Queue_name->Array_size;
 	int *First_element = Queue_name->First_array_element;
 	
-	printf("Addresses:\n");
-	for(i=0; i < Queue_size; i++)
+	printf("Addr: ");
+	for(i=-1; i < Queue_size+1; i++)
 	{
-		printf("%08x ", First_element+i);
+		printf("%16x ", First_element+i);
 	}
-	printf("\n Values:");
-	for(i=0; i < Queue_size; i++)
+	
+	printf("\nValu: ");
+	for(i=-1; i < Queue_size+1; i++)
 	{
-		printf("%08d ", *(First_element+i));
+		printf("%16d ", *(First_element+i));
 	}
+	
+	printf("\nCurr: ");
+	for(i=-1; i < Queue_size+1; i++)
+	{
+		if(Queue_name->Current_element == (First_element+i))
+		{
+			printf("%16d ", 1);
+		}else
+		{
+			printf("%16d ", 0);
+		}
+	}
+	
+	printf("\nNext: ");
+	for(i=-1; i < Queue_size+1; i++)
+	{
+		if(Queue_name->Next_element == (First_element+i))
+		{
+			printf("%16d ", 1);
+		}else
+		{
+			printf("%16d ", 0);
+		}
+	}
+	
+	printf("\nFree space: %d elements", Queue_name->Space_left);
 }
